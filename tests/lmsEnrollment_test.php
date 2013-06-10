383c7df318934a41b58f09f806cc41390ae3d2e0
Jason Peak <jpeak5@lsu.edu>
2013-06-10 11:00:18 -0500

    lmsEn test class clean and ready

--- tests/lmsEnrollment_test.php
+++ tests/lmsEnrollment_test.php
@@ -1,146 +1,40 @@
 <?php
 global $CFG;
 require_once $CFG->dirroot.'/local/ap_report/lib.php';
-//require_once('fixtures/enrollment.php');
-
 
 
 class lmsEnrollment_testcase extends advanced_testcase{
     
-    public $sp;
-    public $numRows;
-    public $start;
-    public $eight_am_yesterday;
-    public $zero_hour_yesterday;
-    public $enrollment;
-    
-    public static $logid =0;
+    public $reportClass;
 
-    public static function strf($arg){
-        return strftime('%F %T',$arg);
-    }
-    
-    public function toTime($in){
-        return strftime('%F %T', $in);
-    }
-    
-    public function dump_time_member(){
-        $rs = $this->sp->report_start;
-        $re = $this->sp->report_end;
-        $ss = $this->sp->survey_start;
-        $se = $this->sp->survey_end;
-
-    }
-    
     public function setUp(){
-        
-        $this->midnight_this_morning = strtotime(strftime('%F', time()));
-        $this->zero_hour_yesterday = $this->midnight_this_morning -86400;
-        $this->eight_am_yesterday = strtotime("+8 hours", $this->zero_hour_yesterday);
-        $this->start = strtotime('-5 day');
+
         $this->resetAfterTest();
         $this->make_dummy_data();
-        
-        global $DB;
-        $this->assertNotEmpty($DB->get_records('enrol_ues_semesters'));
-        $this->sp = new lmsEnrollment();
-        $this->numRows = 10;  
+        $this->reportClass = new lmsEnrollment();
     }
-    
-    public static function gen_id($low=null, $high=null){
-        $low  = isset($low)  ? $low  : 1;
-        $high = isset($high) ? $high : 9999;
-        return rand($low, $high);
-    }
-    
-    public static function gen_idnumber(){
-        return rand(111111111,888999999);
-    }
-    
-    
-    private static function get_workhours_activity_timestamps_for_yesterday($total_time, $count, $start=null, $end=null){
-        assert($count > 0);
-        $times = array();
-        $zero = strtotime(strftime('%F', time())) - 86400; //0:00 yesterday
-        $start = isset($start) ? $start : strtotime("+8 hours",$zero); //yesterday 08:00
-        $end   = isset($end)   ? $end   : strtotime("+8 hours",$start); //yesterday 16:00
-        
-        
-        
-        $random_start_time = rand($start, $end - $total_time); //start in business hours and finish before end
-        
-        $times[0] = $random_start_time;
-        $times[$count-1] = $random_start_time + $total_time;
-        $avg = (int)$total_time/($count-1);
-        
-        //this loop just fills in the gaps
-        $i = $count -2;
-        while($i >0){
-            
-            $times[$i] = $times[$i+1]-(int)$avg;
-            $i--;
-        }
-        assert(count($times) == $count);
-        assert($times[$count-1] - $times[0] == $total_time);
-        return $times;
-    }
-    
-    private function generate_lastaccess(){
-        return rand($this->zero_hour_yesterday-86400, $this->zero_hour_yesterday);
-    }
-    
-    private function generate_previous_cum_timespent(){
-        return rand(0, 1000000);
-    }
-    
-    public static function generate_log_event($time, $userid, $courseid, $is_login=false){
-        
-        $action = $is_login ? 'login' : 'view';
-        $record = array('time'=>$time, 'userid'=>$userid,'course'=>$courseid, 'action'=>$action);
-        
-        return $record;
-    }
-    
-    public static function generate_user_activity_segment($userid, $cum_time_spent, $event_count, $courseid){
-        assert($event_count >= 2);
-        $ts=  self::get_workhours_activity_timestamps_for_yesterday($cum_time_spent, $event_count);
-        
-        //log the user in such that we don't take away from the known time spent
-        $usr_logs = array(self::generate_log_event($ts[0]-10, $userid, $courseid, true));
-        foreach($ts as $usr1_act){
-            $usr_logs[] = self::generate_log_event($usr1_act, $userid, $courseid);
-        }
-        return $usr_logs;
-    }
-    
-//    private function create_ues_semester($id, $year, $name, $campus, $start=null, $grades_due=null){
-//        $start = isset($start) ? $start : strtotime("-7 days", time());
-//        $end   = isset($end)   ? $end   : strtotime("+7 days", time());
-//        return array('id' =>$id, 'year'=>$year,'name'=>$name, 'campus'=>$campus,'classes_start'=> $start, 'grades_due'=>$end);
-//    }
-    
-
     
     private function make_dummy_data(){
-        self::$logid =0;
         global $DB;
+        
         $this->resetAllData();
         $this->resetAfterTest(true);
+
         $DB->delete_records('log');
-        
-        $logs = $DB->get_records('log');
-        $this->assertEmpty($logs);
+        $this->assertEmpty($DB->get_records('log'));
 
         $dataset = $this->createXMLDataSet('tests/fixtures/dataset.xml');
-
         $this->loadDataSet($dataset);
+        
+        $this->assertNotEmpty($DB->get_records('log'));
+        $this->assertNotEmpty($DB->get_records('enrol_ues_semesters'));
     }
     
     public function test_dataset(){
         global $DB;
         //check log activity exists
         $log_sql = "SELECT * FROM {log} WHERE time > ? and time < ?";
-        $logs = $DB->get_records_sql($log_sql, array($this->sp->proc_start, $this->sp->proc_end));
+        $logs = $DB->get_records_sql($log_sql, array($this->reportClass->proc_start, $this->reportClass->proc_end));
         $this->assertNotEmpty($logs);
     }
     
@@ -180,6 +74,9 @@ class lmsEnrollment_testcase extends advanced_testcase{
     }
 
 
+    public function test_construct(){
+        
+    }
 }
 
 
